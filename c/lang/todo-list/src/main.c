6a0a6cfb322159d0d31875483f73d5680a9af872
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-06-10 22:08:57 +0200

    [c/lang/todo-list] Added help flag and updated README

--- c/lang/todo-list/src/main.c
+++ c/lang/todo-list/src/main.c
@@ -4,6 +4,19 @@
 #include "task.h"
 #include "command.h"
 
+int help() {
+    printf("This is a simple todo-list written in C, just to learn the basics of the language.\n");
+    printf("\n");
+    printf("The available commands are listed below:\n");
+    printf("- add       write a task description to create a new task.\n");
+    printf("- check     marks a task as checked.\n");
+    printf("- uncheck   marks a task as unchecked.\n");
+    printf("- drop      drops a task if it is checked.\n");
+    printf("- remove    deletes the tasks file.\n");
+
+    return 0;
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Provide at least one argument.\n");
@@ -13,18 +26,27 @@ int main(int argc, char **argv) {
     char *command = argv[1];
     int exit_code = 0;
 
+    if (strcmp(command, "-h") == 0 || strcmp(command, "--help") == 0) {
+        return help();
+    }
+
     if (strcmp(command, "add") == 0) {
         exit_code = add();
-    } else if (strcmp(command, "check") == 0) {
+    }
+    else if (strcmp(command, "check") == 0) {
         exit_code = edit(argv, task_check);
-    } else if (strcmp(command, "uncheck") == 0) {
+    }
+    else if (strcmp(command, "uncheck") == 0) {
         exit_code = edit(argv, task_uncheck);
-    } else if (strcmp(command, "drop") == 0) {
+    }
+    else if (strcmp(command, "drop") == 0) {
         exit_code = edit(argv, task_drop);
-    } else if (strcmp(command, "remove") == 0) {
+    }
+    else if (strcmp(command, "remove") == 0) {
         exit_code = rm("tasks.csv");
-    } else {
-        printf("The command %s doesn't exist\n", command);
+    }
+    else {
+        printf("The command %s doesn't exist. Please use -h or --help.\n", command);
         exit_code = 1;
     }
    
