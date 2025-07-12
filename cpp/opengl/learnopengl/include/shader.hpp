4b591143245cdb6550408980253d81156cdf1112
Cristian <cristian.marcos@t2o.com>
2025-07-12 23:58:33 +0200

    [cpp/opengl/learnopengl/10_colors] Finished the chapter and added README.md

--- cpp/opengl/learnopengl/include/shader.hpp
+++ cpp/opengl/learnopengl/include/shader.hpp
@@ -95,6 +95,7 @@ public:
     void setVec2(const std::string &name, const glm::vec2 &value) const { 
         glUniform2fv(glGetUniformLocation(id, name.c_str()), 1, &value[0]); 
     }
+
     void setVec2(const std::string &name, float x, float y) const { 
         glUniform2f(glGetUniformLocation(id, name.c_str()), x, y); 
     }
@@ -102,6 +103,7 @@ public:
     void setVec3(const std::string &name, const glm::vec3 &value) const { 
         glUniform3fv(glGetUniformLocation(id, name.c_str()), 1, &value[0]); 
     }
+
     void setVec3(const std::string &name, float x, float y, float z) const { 
         glUniform3f(glGetUniformLocation(id, name.c_str()), x, y, z); 
     }
@@ -109,6 +111,7 @@ public:
     void setVec4(const std::string &name, const glm::vec4 &value) const { 
         glUniform4fv(glGetUniformLocation(id, name.c_str()), 1, &value[0]); 
     }
+
     void setVec4(const std::string &name, float x, float y, float z, float w) const { 
         glUniform4f(glGetUniformLocation(id, name.c_str()), x, y, z, w); 
     }
