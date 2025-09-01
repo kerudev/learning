cb054b73cd6929f9085e8ffaf5819c22129acd23
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-09-02 00:19:50 +0200

    [arduino/book/02_spaceship_interface] Finished the chapter and added README.md

---
+++ arduino/book/02_spaceship_interface/src/main.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  if (digitalRead(2) == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    delay(250);
+
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(250);
+  }
+}
