75d5a92c62f3ec30adfe911b614167e3c90de5f7
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-09-02 00:33:13 +0200

    [arduino/book/03_love_o_meter] Finished the chapter and added README.md

---
+++ arduino/book/03_love_o_meter/README.md
@@ -0,0 +1,6 @@
+# arduino/book/03_love_o_meter
+
+In this project I learned how to:
+- Use the Serial class to print debug info into the Serial monitor.
+- Use the TMP36 temperature sensor.
+- Calculate temperature using TMP36's output.
---
+++ arduino/book/03_love_o_meter/src/main.ino
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+const int SENSOR_PIN = A0;
+
+const float BASELINE_TEMP = 25.0;
+const float VOLTAGE_IN = 5.0;
+
+const int LEDS[] = {2, 3, 4};
+const int LEDS_LEN = sizeof(LEDS) / sizeof(LEDS[0]);
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int n = 0; n < LEDS_LEN; n++) {
+    pinMode(LEDS[n], OUTPUT);
+    digitalWrite(LEDS[n], LOW);
+  }
+}
+
+void loop() {
+  int sensorVal = analogRead(SENSOR_PIN);
+  float volts = (sensorVal / 1024.0) * VOLTAGE_IN;
+  float temp = (volts - 0.5) * 100;
+
+  Serial.print("Sensor value: ");
+  Serial.print(sensorVal);
+  Serial.print(", Volts: ");
+  Serial.print(volts);
+  Serial.print("V, Temperature: ");
+  Serial.print(temp);
+  Serial.println("C");
+
+  int level = (temp - BASELINE_TEMP) / 2;
+
+  // HIGH == 1, LOW == 0
+  for (int n = 0; n < LEDS_LEN; n++)
+    digitalWrite(LEDS[n], n < level);
+
+  delay(1);
+}
