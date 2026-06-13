80c3fa8d4c055f7c78ff09ae951b7fd34eb2fd96
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-06-13 22:04:31 +0200

    [master] Add --committer-date-is-author-date to _scripts/end.sh

--- _scripts/end.sh
+++ _scripts/end.sh
@@ -3,4 +3,4 @@
 git push
 code "$(git rev-parse --show-toplevel)"
 master
-git rebase "$(git rev-parse --symbolic-full-name --abbrev-ref=loose @{-1})"
+git rebase --committer-date-is-author-date "$(git rev-parse --symbolic-full-name --abbrev-ref=loose @{-1})"
